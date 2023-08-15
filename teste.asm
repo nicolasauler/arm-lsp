4b11b705975307836058144a15706963a69d5576
Nicolas Auler <nicolasauler@usp.br>
2023-08-14 23:32:09 -0300

    added images

--- teste.asm
+++ teste.asm
@@ -1,3 +1,12 @@
-L0: mov ax, 0x1234
-    mov bx, 0x5678
-    bnz L0
+L2: MOV ax, 0x1234
+    MOV bx, 0x5678
+    BNE L1
+
+L3: MOV ax, 0x1234
+    MOV bx, 0x5678
+    BNZ L4
+
+L1: ADD r0, r0, r1
+
+L4: MSR CPSR_c, r0
+    MOV pc, lr
